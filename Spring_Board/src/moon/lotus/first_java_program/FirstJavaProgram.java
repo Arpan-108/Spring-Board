e5932e7058d89d235e2f7ff5d699831da5ee15d5
DELL <arpanshah848@gmail.com>
2024-08-12 14:10:43 +0530

    1st Java Program

---
+++ Spring_Board/src/moon/lotus/first_java_program/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+package moon.lotus.first_java_program;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World! Welcome to Java Programming!");
+
+	}
+
+}
